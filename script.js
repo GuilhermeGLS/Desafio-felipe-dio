acea551ea5ca3304c5fa61e981442d5b83a743b4
Guilherme Lima e Silva <guilhermelimaesilva.p@gmail.com>
2023-10-11 21:44:50 -0300

    Desafio feito

---
+++ script.js
@@ -0,0 +1,29 @@
+let heroi = {
+    nome: Guimas,
+    exp: 8000,
+}
+
+if(exp<=1000){
+    console.log('Ferro')
+
+}else if(exp >= 1001 || exp <= 2000){
+    console.log('bronze')
+
+}else if(exp >= 2001 || exp <= 5000){
+    console.log('prata')
+
+}else if(exp >= 6001 || exp <= 7000){
+    console.log('ouro')
+
+}else if(exp >= 7001 || exp <= 8000){
+    console.log('platina')
+
+}else if(exp >= 8001 || exp <= 9000){
+    console.log('Ascendente')
+
+}else if(exp >= 9001 || exp <= 10000){
+    console.log('imortal')
+
+}else if (exp>=10001){
+    console.log('Radiante')
+}
